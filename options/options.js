38fad21d13bb4765008cd233ec74c59b661ed3ba
Crowdbotics <team@crowdbotics.com>
2021-08-04 16:13:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "red_leaf_29457Identifier",
+  url: "https://red_leaf_29457Identifier.botics.co",
+  api: "https://red_leaf_29457Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
